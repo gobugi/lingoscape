5a10137d121458438d8eab2ab6de9ffd85727cbe
johnny <johnpark1987@gmail.com>
2021-10-20 18:29:46 -0700

    finished homepage

--- react-app/src/components/Homepage/Homepage.js
+++ react-app/src/components/Homepage/Homepage.js
@@ -11,10 +11,10 @@ const Homepage = () => {
 
       <Carousel id="slider" infiniteLoop={true} autoPlay={true} showThumbs={false} showArrows={false} showIndicators={false} showStatus={false} interval={5000} stopOnHover={false}>
         <div>
-          <img className="splash-images" alt="img1" src="https://images.unsplash.com/photo-1515378791036-0648a3ef77b2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1770&q=80" />
+          <img className="splash-images" alt="img1" src="https://images.unsplash.com/photo-1483389127117-b6a2102724ae?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1974&q=80" />
         </div>
         <div>
-          <img className="splash-images" alt="img2" src="https://images.unsplash.com/photo-1483389127117-b6a2102724ae?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1974&q=80" />
+          <img className="splash-images" alt="img2" src="https://images.unsplash.com/photo-1515378791036-0648a3ef77b2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1770&q=80" />
         </div>
         <div>
           <img className="splash-images" alt="img3" src="https://images.unsplash.com/photo-1503945438517-f65904a52ce6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80" />
@@ -30,7 +30,21 @@ const Homepage = () => {
         </div>
       </Carousel>
 
-      
+      <div className="message-container">
+        <h1>Explore your new language.</h1>
+        <h2>{`Flashcards for `}
+          <b>all learners</b>
+        </h2>
+        <div className="home-btn-container">
+          <NavLink id="find-btn" to='/languages'>
+            Find Flashcards
+          </NavLink>
+          <NavLink id="make-btn" to='/languages'>
+            Find Flashcards
+          </NavLink>
+        </div>
+      </div>
+
 
     </main>
   )
