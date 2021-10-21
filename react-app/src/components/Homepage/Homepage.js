b4353e8b33f5196ed0718a0f96e231c448325fd2
johnny <johnpark1987@gmail.com>
2021-10-20 17:26:13 -0700

    fixed carousel dimensions as best i could

--- react-app/src/components/Homepage/Homepage.js
+++ react-app/src/components/Homepage/Homepage.js
@@ -9,7 +9,7 @@ const Homepage = () => {
   return (
     <main id="home-main">
 
-      <Carousel id="slider" className="home-content-rotator-inner carousel-inner" infiniteLoop={true} autoPlay={true} showThumbs={false} showArrows={false} showIndicators={false} showStatus={false} interval={5000} stopOnHover={false}>
+      <Carousel id="slider" infiniteLoop={true} autoPlay={true} showThumbs={false} showArrows={false} showIndicators={false} showStatus={false} interval={5000} stopOnHover={false}>
         <div>
           <img className="splash-images" alt="img1" src="https://images.unsplash.com/photo-1515378791036-0648a3ef77b2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1770&q=80" />
         </div>
@@ -29,6 +29,8 @@ const Homepage = () => {
           <img className="splash-images" alt="img6" src="https://images.unsplash.com/photo-1543269664-76bc3997d9ea?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80" />
         </div>
       </Carousel>
+
+      
 
     </main>
   )
