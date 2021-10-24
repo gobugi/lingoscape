d1d4e97f9e38e0493a1af852aa1057da62f66b48
johnny <johnpark1987@gmail.com>
2021-10-24 15:26:02 -0700

    can now add card from deck page

--- react-app/src/components/SingleDeck/SingleDeck.js
+++ react-app/src/components/SingleDeck/SingleDeck.js
@@ -107,9 +107,56 @@ const myOrderedCards = myCards?.sort(function(a, b) {
 });
 
 
+const addCard = async (e) => {
+  e.preventDefault()
+
+  const newCard = {
+    deckId,
+    "question": currentQuestion,
+    "answer": currentAnswer
+  }
+
+
+  const response = await fetch(`/api/cards`, {
+    method: 'POST',
+    body: JSON.stringify(newCard),
+    headers: {
+      "Content-Type": "application/json"
+    }
+  });
+
+  const data = await response.json();
+
+  // const cardsUL = document.getElementById("createdCardsList");
+  // const cardsLi = document.createElement("li");
+  // const cardsSpan1 = document.createElement("span");
+  // const cardsSpan2 = document.createElement("span");
+
+  // cardsUL.appendChild(cardsLi);
+
+  // cardsLi.appendChild(cardsSpan1);
+  // cardsLi.appendChild(cardsSpan2);
+
+  // cardsSpan1.innerHTML = question;
+  // cardsSpan2.innerHTML = answer;
+
+  // const cardsTextInput = document.getElementsByClassName("cardInput");
+  // cardsTextInput.value = '';
+  // cardsTextInput.placeholder = '';
+
+  setCurrentQuestion('');
+  setCurrentAnswer('')
+
+  // document.getElementById("done-btn").style.display="block";
+
+  history.go(0);
+
+  return data
+}
+
+
   return (
     <main id="main-dashboard">
-      <h1>Welcome</h1>
       <h2>{currentTitle && currentTitle}</h2>
       { currentDeck?.authorId === userId && <form id="edit-title" onSubmit={renameDeck}>
         <input
@@ -153,6 +200,36 @@ const myOrderedCards = myCards?.sort(function(a, b) {
         ))}
 
       </ul>
+
+      {(currentDeck?.authorId === userId) &&
+      <ul>
+        <li>
+          <form id="addCardForm" onSubmit={addCard} >
+            <div className='createCard'>
+              {/* <label className='createCardQuestion'>{langArr[languageId - 1]}: </label> */}
+              <input
+                className='textInput cardInput'
+                type="text"
+                // value=""
+                onChange={(e) => setCurrentQuestion(e.target.value)}
+                placeholder='Ex: Bonjour'
+              />
+            </div>
+            <div className='createCard'>
+              {/* <label className='createCardAnswer'>English: </label> */}
+              <input
+                className='textInput cardInput'
+                type="text"
+                // value=""
+                onChange={(e) => setCurrentAnswer(e.target.value)}
+                placeholder='Ex: Hello'
+              />
+            </div>
+            <button className='create-card-btn'>Add Card</button>
+          </form>
+        </li>
+      </ul>
+      }
 
       {(currentDeck?.authorId === userId) &&
         <button onClick={deleteDeck}>Delete Deck</button>
