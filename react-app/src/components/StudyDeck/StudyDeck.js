9c020b93b899809ff9848a7d9853ecad11bafa62
johnny <johnpark1987@gmail.com>
2021-10-26 14:03:12 -0700

    rerenders

--- react-app/src/components/StudyDeck/StudyDeck.js
+++ react-app/src/components/StudyDeck/StudyDeck.js
@@ -47,25 +47,6 @@ const StudyDeck = () => {
         ))
         }
 
-
-        {/* <div>
-          <img className="splash-images" alt="img1" src="https://images.unsplash.com/photo-1483389127117-b6a2102724ae?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1974&q=80" />
-        </div>
-        <div>
-          <img className="splash-images" alt="img2" src="https://images.unsplash.com/photo-1515378791036-0648a3ef77b2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1770&q=80" />
-        </div>
-        <div>
-          <img className="splash-images" alt="img3" src="https://images.unsplash.com/photo-1503945438517-f65904a52ce6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80" />
-        </div>
-        <div>
-          <img className="splash-images" alt="img4" src="https://images.unsplash.com/photo-1543269866-487350d6fa5e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80" />
-        </div>
-        <div>
-          <img className="splash-images" alt="img5" src="https://images.unsplash.com/photo-1521633286323-05b17f47cb74?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1469&q=80" />
-        </div>
-        <div>
-          <img className="splash-images" alt="img6" src="https://images.unsplash.com/photo-1543269664-76bc3997d9ea?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80" />
-        </div> */}
       </Carousel>
 
     </main>
